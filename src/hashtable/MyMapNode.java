efef51966a693c5e800711d9e7fa56aa5bca4cc9
SMITHA RAI L <99192591+SmithaRai@users.noreply.github.com>
2022-04-24 23:38:42 +0530

    Update MyMapNode.java

--- src/hashtable/MyMapNode.java
+++ src/hashtable/MyMapNode.java
@@ -1,5 +1,60 @@
 package hashtable;
 
-public class MyMapNode {
+public class MyMapNode<K,V>  extends MyNode {
+  K key;
+	    V  value;
+	    MyMapNode <K , V> next;
+
+	    public MyMapNode(K key , V value){
+	        this.key =key;
+	        this.value = value;
+	        next=null;
+	    }
+	    @Override
+	    public K getKey() {
+
+	        return key;
+	    }
+
+	    @Override
+	    public void setKey() {
+
+	    }
+
+	    public void setKey(K key) {
+
+	        this.key=key;
+	    }
+
+
+	    public INode<K>getNext() {
+	        // TODO Auto-generated method stub
+	        return next;
+	    }
+
+	    public V getValue() {
+	        return value;
+	    }
+
+
+	    public void setValue(V value) {
+	        this.value=value;
+	    }
+
+
+	    public void setNext(INode<K> next) {
+	        this.next =(MyMapNode<K, V>) next;
+
+	    }
+	    @Override
+	    public String toString() {
+	        StringBuilder MyMapNodeString = new StringBuilder();
+	        MyMapNodeString.append("MyMapNode{" + "K=")
+	                .append(key).append("V=").append(value).append('}');
+	        if(next != null)
+	            MyMapNodeString.append("->").append(next);
+	        return MyMapNodeString.toString();
+	    }
+
 
 }
